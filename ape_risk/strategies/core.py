9bb34ade7496b4ca52ef8d2f9a8aae54102c5610
fmrmf <m@fmrmf.xyz>
2023-01-16 09:27:04 -0500

    feat: multivariate gbms

--- ape_risk/strategies/core.py
+++ ape_risk/strategies/core.py
@@ -135,3 +135,62 @@ def gbms(
         return initial_value * np.exp(np.cumsum(x, axis=0))  # axis=0 sums over rows
 
     return strat.map(pack)
+
+
+@cacheable
+@defines_strategy()
+def multi_gbms(
+    *,
+    initial_value: float,
+    num_points: int,
+    num_rvs: int,
+    params: List,
+    scale: List[List],
+    shift: List,
+    hist_data: Optional[List[List]] = None,
+) -> st.SearchStrategy[npt.ArrayLike]:
+    """
+    Generates instances of ``np.ndarray``. The generated random instances
+    are individual runs of a multivariate Monte Carlo sim driven by
+    correlated Geometric Brownian motions.
+
+    Args:
+        initial_value (float): The initial value of each sim.
+        num_points (int): The number of points to generate for each sim.
+        num_rvs (int): The number of random variables for each sim.
+        params (List): The base parameter arguments (e.g. loc, scale) of the random variables.
+        scale (List[List]): The scale matrix to mix random variables via affine transformation.
+        shift (List): The shift vector to translate random variables via affine transformation.
+        hist_data (Optional[List[List]]): Historical data to fit the random variable params from.
+
+    Returns:
+        :class:`hypothesis.strategies.SearchStrategy[numpy.typing.ArrayLike]`
+    """
+    check_type(float, initial_value, "initial_value")
+    check_type(int, num_points, "num_points")
+    check_type(int, num_rvs, "num_rvs")
+    check_type(list, params, "params")
+    check_type(list, scale, "scale")
+    check_type(list, shift, "shift")
+    if hist_data is not None:
+        check_type(list, hist_data, "hist_data")
+
+        # fit to log differences: log(p[i+1] / p[i])
+        hist_data = np.diff(np.log(np.asarray(hist_data)), axis=0).tolist()
+
+    from ape_risk.strategies.simulation import MultivariateSimulationStrategy
+
+    strat = MultivariateSimulationStrategy(
+        dist_type="norm",
+        num_points=num_points,
+        num_rvs=num_rvs,
+        params=params,
+        scale=scale,
+        shift=shift,
+        hist_data=hist_data,
+    )
+
+    def pack(x: npt.ArrayLike) -> npt.ArrayLike:
+        return initial_value * np.exp(np.cumsum(x, axis=0))  # axis=0 sums over rows
+
+    return strat.map(pack)
