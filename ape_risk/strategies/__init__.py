a3918f8750d0a80f8745afa2803595a8c5382a71
fmrmf <m@fmrmf.xyz>
2023-01-16 11:10:57 -0500

    feat: multi gbms notebook example

--- ape_risk/strategies/__init__.py
+++ ape_risk/strategies/__init__.py
@@ -1,8 +1,10 @@
-from .core import gbms, sims
+from .core import gbms, multi_gbms, multi_sims, sims
 from .simulation import SimulationStrategy
 
 __all__ = [
     "sims",
+    "multi_sims",
     "gbms",
+    "multi_gbms",
     "SimulationStrategy",
 ]
