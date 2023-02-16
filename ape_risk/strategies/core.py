fix: sign error

ape_risk/strategies/core.py
@@ -139,9 +139,9 @@ def gbms(
         check_type(float, r, "r")
 
         # adjust for risk-neutral if given risk-free rate
-        # S_t = S_0 * exp((mu - sigma**2 / 2)*t + sigma * W_t)
-        [mu, sigma] = strat._mc.params.tolist()
-        shift = r - (mu - sigma**2 / 2)
+        # S_t = S_0 * exp(mu_p * t + sigma * W_t); mu_p = mu - sigma**2 / 2
+        [mu_p, sigma] = strat._mc.params.tolist()
+        shift = r - (mu_p + sigma**2 / 2)
 
     def pack(x: npt.ArrayLike) -> npt.ArrayLike:
         return initial_value * np.exp(np.cumsum(np.add(x, shift), axis=0))  # axis=0 sums over rows

tests/strategies/test_core.py
@@ -92,7 +92,7 @@ def test_gbms_risk_neutral_fuzz(p):
     # check distr of p is close to log normal with params
     dlog_p = np.diff(np.log(p.T))
     fit_params = stats.norm.fit(dlog_p)
-    np.testing.assert_allclose(fit_params, [0.0004, 0.005], rtol=2e-1)  # mu tol is not great
+    np.testing.assert_allclose(fit_params, [0.000387, 0.005], rtol=2e-1)  # mu tol is not great
 
 
 @given(strategies.gbms(initial_value=1.0, num_points=100000, params=[0, 1], hist_data=hist_data()))